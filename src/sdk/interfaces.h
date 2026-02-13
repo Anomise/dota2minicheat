5b4a4c4ba65f6a4b9e3bca2d2dcfe8922a8fed66
Anomise <79972271+Anomise@users.noreply.github.com>
2026-02-13 14:52:04 +0500

    Update interfaces.h

--- src/sdk/interfaces.h
+++ src/sdk/interfaces.h
@@ -6,18 +6,27 @@
 
 namespace SDK {
     bool Initialize();
-    void OnFrame(); // вызывать каждый кадр для обновления кешей
+    void OnFrame();
 
-    // Указатели (обновляются в Initialize и OnFrame)
-    inline uintptr_t pGameEntitySystem = 0;
-    inline uintptr_t pGameRules        = 0;
-    inline uintptr_t pLocalPlayer      = 0;
-    inline uintptr_t pViewMatrix       = 0;
-    inline uintptr_t pCameraInstance   = 0;
+    // Pointers
+    inline uintptr_t pGameEntitySystem  = 0;
+    inline uintptr_t pGameRules         = 0;
+    inline uintptr_t pLocalPlayer       = 0;
+    inline uintptr_t pViewMatrix        = 0;
+    inline uintptr_t pCameraInstance    = 0;
 
-    // Кешированные данные (обновляются в OnFrame)
-    inline bool       bInGame          = false;
-    inline Matrix4x4  cachedViewMatrix = {};
+    // Module bases (cached)
+    inline uintptr_t clientBase   = 0;
+    inline uintptr_t clientSize   = 0;
+    inline uintptr_t engineBase   = 0;
+    inline uintptr_t engineSize   = 0;
+
+    // State
+    inline bool bInGame = false;
+    inline Matrix4x4 cachedViewMatrix = {};
+
+    // Interface finding (Source 2 CreateInterface)
+    void* GetInterface(const char* moduleName, const char* ifaceName);
 
     // Accessors
     C_DOTA_BaseNPC_Hero*    GetLocalHero();
@@ -36,7 +45,4 @@ namespace SDK {
     void CollectHeroes(std::vector<HeroData>& outHeroes);
     void CollectAbilities(C_DOTA_BaseNPC* npc, std::vector<AbilityInfo>& outAbilities);
     void CollectItems(C_DOTA_BaseNPC* npc, std::vector<ItemInfo>& outItems);
-
-    // Interface registry helper
-    uintptr_t FindInterface(const char* moduleName, const char* interfaceName);
 }
