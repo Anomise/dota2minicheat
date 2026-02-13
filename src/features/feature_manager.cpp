b1f1284326ddd81289474146a7eb7f2924e878b2
Anomise <79972271+Anomise@users.noreply.github.com>
2026-02-13 14:54:15 +0500

    Update feature_manager.cpp

--- src/features/feature_manager.cpp
+++ src/features/feature_manager.cpp
@@ -1,18 +1,30 @@
-﻿#include "feature_manager.h"
+#include "feature_manager.h"
 #include "camera_hack.h"
 #include "esp.h"
 #include "awareness.h"
 #include "auto_accept.h"
 #include "../config.h"
+#include "../sdk/interfaces.h"
 
 namespace Features {
-    void Initialize() { CameraHack::Initialize(); }
-    void OnFrame() {
-        auto& c = Config::Get();
-        if (c.camerHack) CameraHack::OnFrame();
-        if (c.espEnabled) ESP::OnFrame();
-        if (c.awarenessEnabled) Awareness::OnFrame();
-        if (c.autoAccept) AutoAccept::OnFrame();
+    void Initialize() {
+        CameraHack::Initialize();
     }
-    void Shutdown() { CameraHack::Restore(); }
+
+    void OnFrame() {
+        // Always update SDK state first
+        SDK::OnFrame();
+
+        if (!SDK::IsInGame()) return;
+
+        auto& c = Config::Get();
+        if (c.camerHack)        CameraHack::OnFrame();
+        if (c.espEnabled)       ESP::OnFrame();
+        if (c.awarenessEnabled) Awareness::OnFrame();
+        if (c.autoAccept)       AutoAccept::OnFrame();
+    }
+
+    void Shutdown() {
+        CameraHack::Restore();
+    }
 }
